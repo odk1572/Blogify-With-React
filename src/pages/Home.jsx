a44f0899f1c94535e5b4f131ebc5c2bb3b21dfd1
Om Kariya <161995427+odk1572@users.noreply.github.com>
2024-06-08 17:52:53 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,7 +20,7 @@ function Home() {
                     <div className="flex flex-wrap">
                         <div className="p-2 w-full">
                             <h1 className="text-2xl font-bold hover:text-gray-500">
-                                Welcome to Blogify<br/> Please Login to read posts
+                                Welcome to Blogify<br/> Please Login to read posts<br />Or Signup to create Account
                             </h1>
                         </div>
                     </div>
