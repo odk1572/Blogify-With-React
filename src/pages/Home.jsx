4c46440ede8d270f1393f936256f59dc0409d6b6
Om Kariya <161995427+odk1572@users.noreply.github.com>
2024-06-09 00:32:38 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,7 @@ function Home() {
 
     if (posts.length === 0) {
         return (
-            <div className="w-full py-8 mt-4 text-center text-white">
+            <div className="w-full py-8 mt-4 bg-teal-900 text-center text-white">
                 <Container>
                     <div className="flex flex-wrap">
                         <div className="p-2 w-full">
